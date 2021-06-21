ac519d1042cf6619bf7bbc5474650659b31f8079
SitesHub <76928074+SitesHub@users.noreply.github.com>
2021-06-21 21:01:25 +0200

    Added the middle div, nothing much

--- JS/Pages.js
+++ JS/Pages.js
@@ -1,7 +1,12 @@
 export function indexPage() {
     return (
-        <div class="title">
-            <h2>Welcome to</h2> <h1>The Don't Starve IQ guide</h1>
+        <div>
+            <div class="title">
+                <h2>Welcome to</h2> <h1>The Don't Starve IQ guide</h1>
+            </div>
+            <div class="description">
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate quas ea maxime reiciendis dolorum debitis eveniet aperiam, qui laboriosam mollitia perspiciatis quis quam unde sunt atque sit consequuntur accusamus. Expedita odit, eaque minus ea animi quasi, necessitatibus similique, consequuntur blanditiis ex illo sequi dolore. Aliquam tenetur nihil excepturi eum laudantium quas amet suscipit praesentium, ut sit repudiandae nesciunt possimus quae nemo at officia labore, quo quibusdam ea reiciendis. Maxime, libero praesentium culpa aliquam ab veniam odit commodi sint molestiae perspiciatis voluptatibus quam quod unde provident et repudiandae assumenda ea pariatur, omnis excepturi magni sit saepe eveniet. Doloremque, deleniti minima!</h3>
+            </div>
         </div>
     )
 }
--- Translated_JS/Pages.js
+++ Translated_JS/Pages.js
@@ -1,17 +1,30 @@
 export function indexPage() {
     return React.createElement(
         "div",
-        { "class": "title" },
+        null,
         React.createElement(
-            "h2",
-            null,
-            "Welcome to"
+            "div",
+            { "class": "title" },
+            React.createElement(
+                "h2",
+                null,
+                "Welcome to"
+            ),
+            " ",
+            React.createElement(
+                "h1",
+                null,
+                "The Don't Starve IQ guide"
+            )
         ),
-        " ",
         React.createElement(
-            "h1",
-            null,
-            "The Don't Starve IQ guide"
+            "div",
+            { "class": "description" },
+            React.createElement(
+                "h3",
+                null,
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate quas ea maxime reiciendis dolorum debitis eveniet aperiam, qui laboriosam mollitia perspiciatis quis quam unde sunt atque sit consequuntur accusamus. Expedita odit, eaque minus ea animi quasi, necessitatibus similique, consequuntur blanditiis ex illo sequi dolore. Aliquam tenetur nihil excepturi eum laudantium quas amet suscipit praesentium, ut sit repudiandae nesciunt possimus quae nemo at officia labore, quo quibusdam ea reiciendis. Maxime, libero praesentium culpa aliquam ab veniam odit commodi sint molestiae perspiciatis voluptatibus quam quod unde provident et repudiandae assumenda ea pariatur, omnis excepturi magni sit saepe eveniet. Doloremque, deleniti minima!"
+            )
         )
     );
 }
